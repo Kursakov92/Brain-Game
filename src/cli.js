647865cd748f3ea3a0f042cc97cd274b9c6cedb9
iteco.kursakov <iteco.kursakov@crpt.ru>
2023-08-19 11:52:10 +0300

    добавленно приветствие

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,5 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
-function getUSerName() {
-const userName = readlineSync.question('What is Your name? ');
-console.log(`Hello, ${userName}!\n`); 
-};
-
-export default getUSerName;
+console.log('Welcome to the Brain Games!');
+export const userName = readlineSync.question("May I have your name?");
+ console.log(`Hello, ${userName}!`);
