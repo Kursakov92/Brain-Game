26401cd9ddc91ae89798949de50ed991b8bde2cb
iteco.kursakov <iteco.kursakov@crpt.ru>
2023-08-21 18:55:17 +0300

    улучшил функцию рандома

--- src/cli.js
+++ src/cli.js
@@ -3,11 +3,11 @@ import readlineSync from 'readline-sync';
 function isEven(n) {
   return n % 2 === 0;
 }
-function randomNumber() {
-  return Math.floor(Math.random() * 10);
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 function evenGame() {
-  const number = randomNumber();
+  const number = randomNumber(1, 99);
   console.log(`Answer "yes" if the number is even, otherwise answer "no" - ${number}`);
   const answer = readlineSync.question('Your answer: ');
   const correct = isEven(number) ? 'yes' : 'no';
