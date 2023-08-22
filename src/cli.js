fb50d79fa6edfdd9c57d8ba757581e25ef64d92f
iteco.kursakov <iteco.kursakov@crpt.ru>
2023-08-22 09:20:11 +0300

    перенес even в отделный файл

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,6 @@
 import readlineSync from 'readline-sync';
+import evenGame from '../bin/brain-even.js';
 
-function isEven(n) {
-  return n % 2 === 0;
-}
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-function evenGame() {
-  const number = randomNumber(1, 99);
-  console.log(`Answer "yes" if the number is even, otherwise answer "no" - ${number}`);
-  const answer = readlineSync.question('Your answer: ');
-  const correct = isEven(number) ? 'yes' : 'no';
-  return answer === correct;
-}
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name?');
 console.log(`Hello, ${userName}!`);
