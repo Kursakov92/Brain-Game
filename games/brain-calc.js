70cbd556002b46bea19028512c7a1add4b5140b5
Kursakov Alexnder <sashavalezhnik@yandex.ru>
2023-09-02 11:29:15 +0300

    вынес свитч за пределы функции calc

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -3,27 +3,33 @@ import playGame from '../src/index.js';
 import randomNumber from '../src/randomNumber.js';
 import greeting from '../src/greeting.js';
 
-function calc() {
+const operators = ['+', '-', '*'];
+
+function switchOperator(number1, number2, operator) {
   let correctAnswer;
-  const operator = ['+', '-', '*'];
-  const number1 = randomNumber(1, 99);
-  const number2 = randomNumber(1, 99);
-  const randomOperator = randomNumber(0, 3);
-  console.log('What is the result of the expression?');
-  const answer = readlineSync.question(`Question: ${number1} ${operator[randomOperator]} ${number2}  `);
-  console.log(`Your answer: ${answer}`);
-  switch (randomOperator) {
-    case 0:
+  switch (operator) {
+    case '+':
       correctAnswer = String(number1 + number2);
       break;
-    case 1:
+    case '-':
       correctAnswer = String(number1 - number2);
       break;
-    case 2:
+    case '*':
       correctAnswer = String(number1 * number2);
       break;
     default:
   }
+  return correctAnswer;
+}
+
+function calc() {
+  const number1 = randomNumber(1, 99);
+  const number2 = randomNumber(1, 99);
+  const randomOperator = operators[randomNumber(0, 3)];
+  console.log('What is the result of the expression?');
+  console.log(`Question: ${number1} ${randomOperator} ${number2}  `);
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = switchOperator(number1, number2, randomOperator);
   const result = {
     isCorrect: answer === correctAnswer,
     answer,
