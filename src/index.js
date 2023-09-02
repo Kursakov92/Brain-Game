4fa68eca8434e80f63d741ec123fd795ea514321
Kursakov Alexnder <sashavalezhnik@yandex.ru>
2023-09-02 11:43:50 +0300

    вынес првоерку ответа

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
+function checkAnswer(isCorrect, answer, round, name, correctAnswer) {
+  if (isCorrect) {
+    console.log('Correct!');
+  }
+  if (round === 2) {
+    console.log(`Congratulations, ${name}!`);
+  }
+  if (!isCorrect) {
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
+  }
+}
+
 export default function playGame(game, name) {
   for (let i = 0; i < 3; i += 1) {
     const { isCorrect, answer, correctAnswer } = game();
     const userName = name;
-    if (isCorrect) {
-      console.log('Correct!');
-    }
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-    if (!isCorrect) {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
-      break;
-    }
+    checkAnswer(isCorrect, answer, i, userName, correctAnswer);
+    if (!isCorrect) break;
   }
 }
